b23e9e56f25caf820d239dfcc26959a32c203d05
devon townsend <devonstownsend@gmail.com>
2014-08-29 20:31:34 +0800

    attached weather api. now can call getWeatherData with coordinates

--- parseCloud/cloud/main.js
+++ parseCloud/cloud/main.js
@@ -5,64 +5,50 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world! Weather app checking in!");
 });
 
-Parse.Cloud.define("getInstagramURL", function(request, response) {
+Parse.Cloud.define("getStaticInstagramURL", function(request, response) {
   response.success("http://photos-c.ak.instagram.com/hphotos-ak-xaf1/10624164_1512601182319290_958640297_n.jpg");
 });
 
-Parse.Cloud.beforeSave("IGPhoto", function(request, response) {
-    var igPhoto = request.object;
-    var imageURL;
-    igPhoto.get('fromUser').fetch().then(function(user) {
- 
-        fromUsername = user.get('username');
-        return challenge.get('toUser').fetch();
- 
-    }).then(function(user) {
- 
-        toUsername = user.get('username');
- 
-        var firstQuery = new Parse.Query("TBChallenge");
-        firstQuery.include("fromUser");
-        firstQuery.include("toUser");
- 
-        var fromInnerQuery = new Parse.Query("_User");
-        fromInnerQuery.equalTo("username", fromUsername);
-        firstQuery.matchesQuery("fromUser", fromInnerQuery);
- 
-        var toInnerQuery = new Parse.Query("_User");
-        toInnerQuery.equalTo("username", toUsername);
-        firstQuery.matchesQuery("toUser", toInnerQuery);
- 
-        return firstQuery.find();
- 
-    }).then(function(results) {
-        if (results.length > 0) {
-            response.error('Duplicate challenge');
-        } else {
-            response.success();
-        };
-    });
+
+Parse.Cloud.define("getWeatherData", function(request, response) {
+  getWeatherData(request, response);
 });
 
+//api.openweathermap.org/data/2.5/weather?lat=35&lon=139
 
+function getWeatherData(request, response) {
 
-Parse.Cloud.define("weatherData", function(request, response) {
+  var lat = request.params.lat;
+  var lon = request.params.lon;
   
-  var query = new Parse.Query("Review");
-  query.equalTo("movie", request.params.movie);
-  query.find({
-    success: function(results) {
-      var sum = 0;
-      for (var i = 0; i < results.length; ++i) {
-        sum += results[i].get("stars");
-      }
-      response.success(sum / results.length);
+  console.log('getWeatherData called. lat= '+request.params.lat);
+
+  Parse.Cloud.httpRequest({
+    url: 'api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'',
+    success: function(httpResponse) {
+      console.log('the response is ' + httpResponse.text);
+
+      obj = JSON.parse(httpResponse.text);
+
+      console.log('description is ' + obj.weather[0].description);
+      console.log('base is ' + obj.base);
+      console.log('temp is ' + obj.main.temp);
+      console.log('high is ' + obj.main.temp_max);
+      console.log('low is ' + obj.main.temp_min);
+
+      response.success('succeeded');
     },
-    error: function() {
-      response.error("weather lookup failed");
+    error: function(httpResponse) {
+      response.error('Request failed with response code ' + httpResponse.status);
     }
   });
-});
+}
+
+function ParseWeatherJSON(weatherJSON) {
+
+
+}
+
 
 
 
