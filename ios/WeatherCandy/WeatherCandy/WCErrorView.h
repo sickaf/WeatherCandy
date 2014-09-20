08d07cc812af61e68e2a69fe18d201c2cbf64d0d
Cody <cody.ko@gmail.com>
2014-09-20 16:40:37 +1000

    implemented error handling on the client

---
+++ ios/WeatherCandy/WeatherCandy/WCErrorView.h
@@ -0,0 +1,19 @@
+//
+//  WCErrorView.h
+//  WeatherCandy
+//
+//  Created by Cody Kolodziejzyk on 9/19/14.
+//  Copyright (c) 2014 sickaf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCErrorView : UIView
+
+@property (nonatomic, strong) UILabel *lbl;
+@property (nonatomic, strong) UIButton *butt;
+
+@property (nonatomic, strong) UIView *spacer1;
+@property (nonatomic, strong) UIView *spacer2;
+
+@end
